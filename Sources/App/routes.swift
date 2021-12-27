1c4c9c050c9cc8ea0fa345bb6fa8dcda978fd2b2
Ivan Sapozhnik <sapozhnik.ivan@gmail.com>
2021-12-27 11:33:59 +0100

    clean up code

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -61,23 +61,22 @@ func routes(_ app: Application) throws {
             return try? response.content.decode(LoginResponse.self)
         }
     
-        
-        let aaa = loginData.map { res -> EventLoopFuture<RCOverview> in
+        let login = loginData.map { res -> EventLoopFuture<RCOverview> in
             let authToken = res?.authentication_token ?? ""
             headers.add(name: "Cookie", value: "rc_auth_token=\(authToken)")
             let res = req.client.get("https://api.revenuecat.com/v1/developers/me/overview?app=", headers: headers)
-            let bbb = res.flatMapThrowing { response in
+            let overview = res.flatMapThrowing { response in
                 return try response.content.decode(RCOverview.self)
             }
 
-            return bbb
+            return overview
         }
         let formatter = NumberFormatter()
         formatter.locale = Locale.current
         formatter.numberStyle = .currency
         formatter.currencySymbol = ""
         
-        return aaa.flatMap { futureLoop in
+        return login.flatMap { futureLoop in
             futureLoop.map { overview in
                 let revenue = formatter.string(from: overview.revenue as NSNumber) ?? "0"
                 
@@ -90,23 +89,5 @@ func routes(_ app: Application) throws {
                 ])
             }
         }
-    }
-    
-    /**
-     
-     {
-         "active_subscribers_count": 4,
-         "active_trials_count": 2,
-         "active_users_count": 353,
-         "installs_count": 243,
-         "mrr": 3.5521728951546,
-         "revenue": 158.409177242769
-     }
-     
-     
-     */
-
-    app.get("hello") { req -> String in
-        return "Hello, world!"
     }
 }
